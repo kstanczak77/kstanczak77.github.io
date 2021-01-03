350a94f18758a4c27a0ae1e51742901aad1a4bae
kstanczak77 <kstanczak77@users.noreply.github.com>
2021-01-03 23:01:08 +0100

    jsdom

--- zadania-domowe/JS/js/script.js
+++ zadania-domowe/JS/js/script.js
@@ -1,1 +1,23 @@
-console.log("Moje imię to Krzysztof")
+// console.log("Moje imię to Krzysztof");
+//
+let name = [''];
+let strName = "akademia108";
+let rev = name.reverse();
+let strArray = rev.join('');
+
+
+console.log(strArray);
+
+const reverseString = (str) => {
+    let strArray = str.split('');
+    let rev = strArray.reverse();
+    let returnString = rev.join("");
+    return returnString;
+
+}
+
+
+console.log(reverseString(strName));
+
+/* console.log(reverseString(strName));
+console.log(reverseString(strName2));  */
