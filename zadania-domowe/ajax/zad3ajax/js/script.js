f46c83e4768d4c041efa73fccae279b8054d3711
kstanczak77 <kstanczak77@users.noreply.github.com>
2021-01-16 19:03:39 +0100

    zad3ajax

---
+++ zadania-domowe/ajax/zad3ajax/js/script.js
@@ -0,0 +1,54 @@
+console.log('xxx')
+
+$(document).ready(function() {
+    $('#get-data').click(function() {
+    //     // 1 metoda get
+    //    $.get('https://akademia108.pl/api/ajax/get-post.php').done(function(data) { 
+            
+    //         let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+    //         let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+    //         let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+    //         let pBody = $('<p></p>').text(`Body: ${data.body}`);
+    //         let hr = $('<hr />');
+            
+    //         let jqBody = $('body');
+            
+    //         jqBody.append(pId);
+    //         jqBody.append(pUserId);
+    //         jqBody.append(pTitle);
+    //         jqBody.append(pBody);
+    //         jqBody.append(hr);
+
+    //         // console.log(data);
+
+    //     })
+    //     .fail(function(error) {
+    //         console.error(error);    
+    //     });
+    // });
+   
+        // 2 metoda get json
+        $.getJSON('https://akademia108.pl/api/ajax/get-post.php').done(function(data) { 
+             
+             let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+             let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+             let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+             let pBody = $('<p></p>').text(`Body: ${data.body}`);
+             let hr = $('<hr />');
+             
+             let jqBody = $('body');
+             
+             jqBody.append(pId);
+             jqBody.append(pUserId);
+             jqBody.append(pTitle);
+             jqBody.append(pBody);
+             jqBody.append(hr);
+ 
+             // console.log(data);
+ 
+         })
+         .fail(function(error) {
+             console.error(error);    
+         });
+     });
+});
